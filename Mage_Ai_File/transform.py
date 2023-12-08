74164ea4fa0c6209e7d4667e24551c48d4eed1a0
Chayawee_N <114067408+ChayaNiyom@users.noreply.github.com>
2023-12-08 18:21:44 +0700

    Update transform.py

--- Mage_Ai_File/transform.py
+++ Mage_Ai_File/transform.py
@@ -41,13 +41,6 @@ def transform(df, *args, **kwargs):
     datetime_dim = datetime_dim[['datetime_id', 'tpep_pickup_datetime', 'pick_hour', 'pick_day', 'pick_month', 'pick_year', 'pick_weekday',
                              'tpep_dropoff_datetime', 'drop_hour', 'drop_day', 'drop_month', 'drop_year', 'drop_weekday']]
 
-    passenger_count_dim = df[['passenger_count']].drop_duplicates().reset_index(drop=True)
-    passenger_count_dim['passenger_count_id'] = passenger_count_dim.index
-    passenger_count_dim = passenger_count_dim[['passenger_count_id','passenger_count']]
-
-    trip_distance_dim = df[['trip_distance']].drop_duplicates().reset_index(drop=True)
-    trip_distance_dim['trip_distance_id'] = trip_distance_dim.index
-    trip_distance_dim = trip_distance_dim[['trip_distance_id','trip_distance']]
     rate_code_type = {
         1:"Standard rate",
         2:"JFK",
@@ -62,16 +55,6 @@ def transform(df, *args, **kwargs):
     rate_code_dim['rate_code_name'] = rate_code_dim['RatecodeID'].map(rate_code_type)
     rate_code_dim = rate_code_dim[['rate_code_id','RatecodeID','rate_code_name']]
 
-
-    pickup_location_dim = df[['pickup_longitude', 'pickup_latitude']].drop_duplicates().reset_index(drop=True)
-    pickup_location_dim['pickup_location_id'] = pickup_location_dim.index
-    pickup_location_dim = pickup_location_dim[['pickup_location_id','pickup_latitude','pickup_longitude']] 
-
-
-    dropoff_location_dim = df[['dropoff_longitude', 'dropoff_latitude']].drop_duplicates().reset_index(drop=True)
-    dropoff_location_dim['dropoff_location_id'] = dropoff_location_dim.index
-    dropoff_location_dim = dropoff_location_dim[['dropoff_location_id','dropoff_latitude','dropoff_longitude']]
-
     payment_type_name = {
         1:"Credit card",
         2:"Cash",
@@ -85,24 +68,27 @@ def transform(df, *args, **kwargs):
     payment_type_dim['payment_type_name'] = payment_type_dim['payment_type'].map(payment_type_name)
     payment_type_dim = payment_type_dim[['payment_type_id','payment_type','payment_type_name']]
 
-    fact_table = df.merge(passenger_count_dim, on='passenger_count') \
-             .merge(trip_distance_dim, on='trip_distance') \
-             .merge(rate_code_dim, on='RatecodeID') \
-             .merge(pickup_location_dim, on=['pickup_longitude', 'pickup_latitude']) \
-             .merge(dropoff_location_dim, on=['dropoff_longitude', 'dropoff_latitude'])\
-             .merge(datetime_dim, on=['tpep_pickup_datetime','tpep_dropoff_datetime']) \
-             .merge(payment_type_dim, on='payment_type') \
-             [['VendorID', 'datetime_id', 'passenger_count_id',
-               'trip_distance_id', 'rate_code_id', 'store_and_fwd_flag', 'pickup_location_id', 'dropoff_location_id',
-               'payment_type_id', 'fare_amount', 'extra', 'mta_tax', 'tip_amount', 'tolls_amount',
-               'improvement_surcharge', 'total_amount']]
+    vendor_name = {
+    1:"Creative Mobile Technologies, LLC",
+    2:"VeriFone Inc."
+    }
+    vendor_dim = df[['VendorID']].reset_index(drop=True)
+    vendor_dim['vendor_id'] = vendor_dim.index
+    vendor_dim['vendor_name'] = vendor_dim['VendorID'].map(vendor_name)
+    vendor_dim = vendor_dim[['vendor_id','VendorID','vendor_name']]
+
+    fact_table = df.merge(vendor_dim, left_on='trip_id', right_on='vendor_id') \
+                 .merge(rate_code_dim, left_on='trip_id', right_on='rate_code_id') \
+                 .merge(datetime_dim, left_on='trip_id', right_on='datetime_id') \
+                 .merge(payment_type_dim, left_on='trip_id', right_on='payment_type_id') \
+                 [['trip_id','vendor_id', 'datetime_id', 'passenger_count',
+                   'trip_distance', 'rate_code_id', 'store_and_fwd_flag', 'pickup_longitude', 'pickup_latitude',
+                   'dropoff_longitude', 'dropoff_latitude', 'payment_type_id', 'fare_amount', 'extra', 'mta_tax',
+                   'tip_amount', 'tolls_amount', 'improvement_surcharge', 'total_amount']]
 
     return {"datetime_dim":datetime_dim.to_dict(orient="dict"),
-    "passenger_count_dim":passenger_count_dim.to_dict(orient="dict"),
-    "trip_distance_dim":trip_distance_dim.to_dict(orient="dict"),
+
     "rate_code_dim":rate_code_dim.to_dict(orient="dict"),
-    "pickup_location_dim":pickup_location_dim.to_dict(orient="dict"),
-    "dropoff_location_dim":dropoff_location_dim.to_dict(orient="dict"),
     "payment_type_dim":payment_type_dim.to_dict(orient="dict"),
     "fact_table":fact_table.to_dict(orient="dict")}
 
